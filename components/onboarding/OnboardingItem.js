9bd565d1724772ebf11c3d184d3a02f084c6b115
John Nicholas Suharjono <john.suharjono10@gmail.com>
2023-03-12 23:32:05 +0800

    update onboarding screen

--- components/onboarding/OnboardingItem.js
+++ components/onboarding/OnboardingItem.js
@@ -8,7 +8,7 @@ const OnboardingItem = ({ item }) => {
     <View style={{ width, alignItems: "center", padding: 20 }}>
       <View style={{ flex: 0.65, justifyContent: "center" }}>
         <Image
-          source={{ uri: item.image }}
+          source={item.image}
           style={{
             width: width / 2,
             height: height / 2,
@@ -29,13 +29,13 @@ export default OnboardingItem
 const styles = StyleSheet.create({
   onboardingTitle: {
     fontWeight: "800",
-    fontSize: 28,
+    fontSize: 32,
     textAlign: "center",
     marginBottom: 10,
   },
   onboardingSubtitle: {
     fontWeight: "300",
     textAlign: "center",
-    fontSize: 16,
+    fontSize: 26,
   },
 })
